11c3968c1adc291663ce46eb8bccc436a825e24e
Abdi urgessa <abdiurgessa9@gmail.com>
2022-04-29 23:54:24 +0300

    convertedtonodefile

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000')
+app.listen(5000, () => {
+  console.log('App listening on port 5000')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
